6c2a1c1f65f71572dd8505e979a40b40e9817366
hieubq <hieubq90@gmail.com>
2017-07-29 23:28:09 +0700

    first commit

---
+++ config/polyfills.js
@@ -0,0 +1,11 @@
+// fetch() polyfill for making API calls.
+require('whatwg-fetch');
+
+// es6 feature as promiss, object-assign ...
+require('core-js/es6');
+
+// Web standards: setTimeout / setInterval
+require('core-js/web');
+
+// Non-standard:
+require('core-js/core');
